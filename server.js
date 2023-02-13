0d77a1a2398165762202d31b327e99661151eb08
Usman AR <usman8529@gmail.com>
2023-02-13 23:50:50 +0530

    Integration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ejs = require('ejs')
 const bodyParser = require('body-parser');
 const Product = require('./schema');
 const port =  process.env.PORT || 5000;
@@ -9,6 +10,11 @@ const product = require('product');
 app.use(bodyParser.json());
 
 
+app.use(express.static('views/static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine','ejs');
+    
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/products', {
@@ -18,22 +24,45 @@ mongoose.connect('mongodb://localhost/products', {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
+// // View products
+// app.get('/products', async (req, res) => {
+//   try {
+//     const product = new Product(req.body);
+//     const savedProduct = await product.save();
+//     res.json(savedProduct);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// });
+
 // Add a product
-app.post('/products', async (req, res) => {
-  try {
-    const product = new Product(req.body);
-    const savedProduct = await product.save();
-    res.json(savedProduct);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+app.get('/products/add',async(req,res)=>{
+        res.render('add',{})
+})
+
+// Add a product
+app.post('/products/add',async(req,res)=>{
+    try{
+    const product= new Product({
+        productId:req.body.id,
+        name:req.body.name,
+        price:req.body.price,
+        featured:   Boolean(req.body.featured),
+        rating:req.body.rating,
+        createdAt:req.body.createdAt,
+        company:req.body.company
+    })
+    product.save();
+    res.redirect('/products')
+}
+catch(err){
+    res.send(err.message)
+}
+})
 
 
-
-
- // Fetch products with less than certain price 
- app.get('/products/',async (req,res)=>{
+ // Fetch products
+ app.get('/products',async (req,res)=>{
     try{
     var products;
     const price = req.query.price;
@@ -51,8 +80,10 @@ app.post('/products', async (req, res) => {
     else {
          products=await Product.find();
     }
-    
-    res.json(products);
+    res.render('main',{
+        products:products
+    })
+    // res.json(products);
     }
     catch(err){
         res.status(500).json({message:err.message});
